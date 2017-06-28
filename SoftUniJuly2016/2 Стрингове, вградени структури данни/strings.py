6af43967b8e679268b49b2e96f3a848b325d3856
svet <svet@Aspire-E1-571G>
2017-06-28 18:43:44 +0300

    2. Стрингове

--- SoftUniJuly2016/2 Стрингове, вградени структури данни/strings.py
+++ SoftUniJuly2016/2 Стрингове, вградени структури данни/strings.py
@@ -1,3 +1,14 @@
 # https://www.youtube.com/watch?v=C49kY2DxaSo
 
-print('assaasd')
+name = 'ivan'
+print(len(name)) #daljina
+surName = 'vankov'
+print(name + ' ' + surName) #concatination
+
+for ch in name:
+    print(ch) #ciklim bukvi ot stringa
+    #print(ch, end='n')
+print(name[0])
+print(name[-3])
+print(name.find('va'))
+print(name.find('ov'))
